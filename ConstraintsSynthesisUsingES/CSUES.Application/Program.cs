b12a7218b11d5b86cca9a2e73d136a868f6081e5
Arkadiusz Chorian <arkadiusz.chorian@gmail.com>
2017-08-18 00:55:33 +0200

    Console application implemented.

--- ConstraintsSynthesisUsingES/CSUES.Application/Program.cs
+++ ConstraintsSynthesisUsingES/CSUES.Application/Program.cs
@@ -1,9 +1,117 @@
-﻿namespace CSUES.Application
+﻿using System;
+using System.Linq;
+using CSUES.Common;
+using CSUES.Engine.Enums;
+using CSUES.Engine.Factories;
+using CSUES.Engine.Measurement;
+using CSUES.Engine.Models;
+using CSUES.Engine.PointsGeneration;
+using ES.Core.Enums;
+using ES.Core.Models;
+using EvolutionDefaults = ES.Core.Utils.Defaults;
+using Defaults = CSUES.Engine.Utils.Defaults;
+using Version = CSUES.Common.Version;
+
+namespace CSUES.Application
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var database = new DatabaseContext("database1.db");
+            var version = new Version(DateTime.Now);
+            var experimentParameters = GetExperimentParameters();
+
+            //if (database.Exists(experimentParameters))
+            //{
+            //    Console.WriteLine("BYLEM WIDZIALEM");
+            //    //database.Dispose();
+            //    return;
+            //}
+
+            database.Insert(version);
+            database.Insert(experimentParameters);
+            database.Insert(experimentParameters.EvolutionParameters);
+
+            try
+            {
+                var enginesFactory = new EnginesFactory();
+
+                var engine = enginesFactory.Create(experimentParameters);
+
+                var distanceCalculator = new CanberraDistanceCalculator();
+                var positivePointsGenerator = new PositivePointsGenerator();
+                var positiveTrainingPoints = positivePointsGenerator.GeneratePoints(experimentParameters.NumberOfPositivePoints, engine.Benchmark.Domains, engine.Benchmark.Constraints);
+
+                var negativePointsGenerator = new NegativePointsGenerator(positiveTrainingPoints, distanceCalculator, new NearestNeighbourDistanceCalculator(distanceCalculator));
+                var negativeTrainingPoints = negativePointsGenerator.GeneratePoints(experimentParameters.NumberOfNegativePoints, engine.Benchmark.Domains);
+
+                var trainingPoints = positiveTrainingPoints.Concat(negativeTrainingPoints).ToArray();     
+                var mathModel = engine.SynthesizeModel(trainingPoints);
+
+                database.Insert(mathModel);
+
+                var testPointsGenerator = new TestPointsGenerator();
+                var testPoints = testPointsGenerator.GeneratePoints(experimentParameters.NumberOfTestPoints, engine.Benchmark.Domains, engine.Benchmark.Constraints);
+
+                var statistics = engine.EvaluateModel(testPoints);
+
+                database.Insert(statistics);
+            }
+            catch (Exception exception)
+            {
+                database.Insert(exception);
+            }
+
+            database.Save();
+            database.Dispose();
+        }
+
+        private static ExperimentParameters GetExperimentParameters()
+        {
+            var numberOfDimensions = Arguments.Get<int>(nameof(ExperimentParameters.NumberOfDimensions));
+
+            return new ExperimentParameters(
+                numberOfDimensions: numberOfDimensions,
+                basePopulationSize: Arguments.Get<int>(nameof(EvolutionParameters.BasePopulationSize)),
+                offspringPopulationSize: Arguments.Get<int>(nameof(EvolutionParameters.OffspringPopulationSize)),
+                numberOfGenerations: Arguments.Get<int>(nameof(EvolutionParameters.NumberOfGenerations)),
+                seed: Arguments.Get<int>(nameof(ExperimentParameters.Seed)),
+                typeOfBenchmark: Arguments.Get<BenchmarkType>(nameof(ExperimentParameters.TypeOfBenchmark)),
+
+                trackEvolutionSteps: Arguments.Get(nameof(ExperimentParameters.UseRedundantConstraintsRemoving), EvolutionDefaults.TrackEvolutionSteps),
+                useRedundantConstraintsRemoving: Arguments.Get(nameof(ExperimentParameters.UseRedundantConstraintsRemoving), Defaults.UseRedundantConstraintsRemoving),
+                useDataNormalization: Arguments.Get(nameof(ExperimentParameters.UseDataNormalization), Defaults.UseDataNormalization),
+                allowQuadraticTerms: Arguments.Get(nameof(ExperimentParameters.AllowQuadraticTerms), Defaults.AllowQuadraticTerms),      
+                        
+                ballnBoundaryValue: Arguments.Get(nameof(ExperimentParameters.BallnBoundaryValue), Defaults.BallnBoundaryValue),
+                cubenBoundaryValue: Arguments.Get(nameof(ExperimentParameters.CubenBoundaryValue), Defaults.CubenBoundaryValue),
+                simplexnBoundaryValue: Arguments.Get(nameof(ExperimentParameters.SimplexnBoundaryValue), Defaults.SimplexnBoundaryValue),
+
+                numberOfDomainSamples: Arguments.Get(nameof(ExperimentParameters.NumberOfDomainSamples), Defaults.NumberOfDomainSamples),
+                numberOfTestPoints: Arguments.Get(nameof(ExperimentParameters.NumberOfTestPoints), Defaults.NumberOfTestPoints),
+                numberOfPositivePoints: Arguments.Get(nameof(ExperimentParameters.NumberOfPositivePoints), Defaults.NumberOfPositivePoints),
+                numberOfNegativePoints: Arguments.Get(nameof(ExperimentParameters.NumberOfNegativePoints), Defaults.NumberOfNegativePoints),
+                maxNumberOfPointsInSingleArray: Arguments.Get(nameof(ExperimentParameters.MaxNumberOfPointsInSingleArray), Defaults.MaxNumberOfPointsInSingleArray),
+
+                globalLearningRate: Arguments.Get(nameof(EvolutionParameters.GlobalLearningRate), EvolutionDefaults.GlobalLearningRate(numberOfDimensions)),
+                individualLearningRate: Arguments.Get(nameof(EvolutionParameters.IndividualLearningRate), EvolutionDefaults.IndividualLearningRate(numberOfDimensions)),
+                stepThreshold: Arguments.Get(nameof(EvolutionParameters.StepThreshold), EvolutionDefaults.StepThreshold),
+                rotationAngle: Arguments.Get(nameof(EvolutionParameters.RotationAngle), EvolutionDefaults.RotationAngle),
+                typeOfMutation: Arguments.Get(nameof(EvolutionParameters.TypeOfMutation), EvolutionDefaults.TypeOfMutation),
+
+                numberOfParentsSolutionsToSelect: Arguments.Get(nameof(EvolutionParameters.NumberOfParentsSolutionsToSelect), EvolutionDefaults.NumberOfParentsSolutionsToSelect),
+                typeOfParentsSelection: Arguments.Get(nameof(EvolutionParameters.TypeOfParentsSelection), ParentsSelectionType.Even),
+                typeOfSurvivorsSelection: Arguments.Get(nameof(EvolutionParameters.TypeOfSurvivorsSelection), EvolutionDefaults.TypeOfSurvivorsSelection),
+
+                oneFifthRuleCheckInterval: Arguments.Get(nameof(EvolutionParameters.OneFifthRuleCheckInterval), EvolutionDefaults.OneFifthRuleCheckInterval),
+                oneFifthRuleScalingFactor: Arguments.Get(nameof(EvolutionParameters.OneFifthRuleScalingFactor), EvolutionDefaults.OneFifthRuleScalingFactor),
+
+                useRecombination: Arguments.Get(nameof(EvolutionParameters.UseRecombination), EvolutionDefaults.UseRecombination),
+                typeOfObjectsRecombination: Arguments.Get(nameof(EvolutionParameters.TypeOfObjectsRecombination), EvolutionDefaults.TypeOfObjectsRecombination),
+                typeOfStdDeviationsRecombination: Arguments.Get(nameof(EvolutionParameters.TypeOfStdDeviationsRecombination), EvolutionDefaults.TypeOfStdDeviationsRecombination),
+                typeOfRotationsRecombination: Arguments.Get(nameof(EvolutionParameters.TypeOfRotationsRecombination), EvolutionDefaults.TypeOfRotationsRecombination)
+                );
         }
     }
 }
